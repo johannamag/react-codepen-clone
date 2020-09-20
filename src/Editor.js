798ae104207db6f15fbae203b093f796b8d6ef4b
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-20 17:57:55 +0200

    Added Open and Close Button

--- src/Editor.js
+++ src/Editor.js
@@ -5,19 +5,28 @@ import "codemirror/mode/xml/xml";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
 import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default function Editor(props) {
   const { language, displayName, value, onChange } = props;
+  const [open, setOpen] = useState(true);
 
   function handleChange(editor, data, value) {
     onChange(value);
   }
 
   return (
-    <div className="editor-container">
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
       <div className="editor-title">
         {displayName}
-        <button></button>
+        <button
+          type="button"
+          className="ex-coll-btn"
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
       </div>
       <ControlledEditor
         onBeforeChange={handleChange}
